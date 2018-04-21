8db90295d9d0f4dae5cd8c19b08248d2e83be51f
UdonDa <udoooon0727@gmail.com>
2018-04-21 23:38:53 +0900

    [Add]2-4

---
+++ tutorials/02-intermediate/bidirectional_recurrent_neural_network/own.py
@@ -0,0 +1,88 @@
+import torch 
+import torch.nn as nn
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+
+# Hyper Parameters
+sequence_length = 28
+input_size = 28
+hidden_size = 128
+num_layers = 2
+num_classes = 10
+batch_size = 300
+num_epochs = 20
+learning_rate = 3e-3
+
+train_dataset = dsets.MNIST(
+    root = './data/',
+    train = True,
+    transform = transforms.ToTensor(),
+    download = True
+)
+test_dataset = dsets.MNIST(
+    root = './data/',
+    train = False,
+    transform = transforms.ToTensor()
+)
+
+train_loader = torch.utils.data.DataLoader(
+    dataset = train_dataset,
+    batch_size = batch_size,
+    shuffle = True
+)
+test_loader = torch.utils.data.DataLoader(
+    dataset = test_dataset,
+    batch_size = batch_size,
+    shuffle = False
+)
+
+# Model
+class BiRNN(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(BiRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, bidirectional=True)
+        self.fc = nn.Linear(hidden_size*2, num_classes)
+
+    def forward(self, x):
+        h0 = Variable(torch.zeros(self.num_layers*2, x.size(0), self.hidden_size)).cuda()
+        c0 = Variable(torch.zeros(self.num_layers*2, x.size(0), self.hidden_size)).cuda()
+
+        out, _ = self.lstm(x, (h0, c0))
+        out = self.fc(out[:, -1, :])
+        return out
+
+rnn = BiRNN(input_size, hidden_size, num_layers, num_classes)
+rnn.cuda()
+
+# Loss and Optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(rnn.parameters(), lr=learning_rate)
+
+# Train
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        images = Variable(images.view(-1, sequence_length, input_size)).cuda()
+        labels = Variable(labels).cuda()
+
+        optimizer.zero_grad()
+        outputs = rnn(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        if (i+1) % 100 == 0:
+            print("Epoch: {}/{}, Step: {}/{}, Loss: {}".format(epoch+1, num_epochs, i+1, len(train_dataset) // batch_size, loss.data[0]))
+
+# Test
+correct, total = 0, 0
+for images, labels in test_loader:
+    images = Variable(images.view(-1, sequence_length,input_size)).cuda()
+    outputs = rnn(images)
+    _, predicted = torch.max(outputs.data, 1)
+    total += labels.size(0)
+    correct += (predicted.cpu() == labels).sum()
+
+print("Accuracy: {}".format(100 * correct / total))
